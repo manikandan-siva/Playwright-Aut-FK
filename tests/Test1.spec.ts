766cb8ca895db15262a363272b349be5239e70a6
Sam <manikandan.siva@ymail.com>
2026-06-07 12:48:28 +0530

    Day-1

---
+++ tests/Test1.spec.ts
@@ -0,0 +1,13 @@
+let username:string="mani";
+let age:number = 25;
+let log:boolean = false;
+
+interface user {
+    username : string;
+    password : string;
+}
+
+const testUser: user ={
+    username:"adi",
+    password:"ad123"
+};
