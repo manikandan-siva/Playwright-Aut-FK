705a397fc0a71e76633d357d89e901140b4d90fe
Sam <manikandan.siva@ymail.com>
2026-06-09 15:56:29 +0530

    api day 1

--- tests/api/api1.spec.ts
+++ tests/api/api1.spec.ts
@@ -1,8 +1,30 @@
 import {test,expect} from '@playwright/test';
-test.describe('User API',()=>{
+test.describe.serial('User API',()=>{
+     let req : any;
+       let res: any;
+       let status: number;
+    test.beforeAll(async({request})=>{
+   req=await request.get('https://reqres.in/api/users/1', {
+            headers: {
+                'Content-Type': 'application/json',
+                'x-api-key': 'free_user_3EtVCaF2Tfp3zv83DOpHJXBI1X4' // Replace with your actual key
+                
+            }});
+   status=req.status();
+   res=await req.json();
+   
+        });
     test('Req1',async({request})=>{
-        expect((await request.get('https://reqres.in/api/users')).status()).toBe(200);
+          expect(status).toBe(200);
+        
+        console.log(res);
+        console.log(res.page);
+        console.log(res.data.length)
     })
+
+    test('User1',async({request})=>{
+    
+})
 }
 
 )
